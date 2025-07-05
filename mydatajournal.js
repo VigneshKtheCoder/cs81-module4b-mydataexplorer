ec938c8fea0f8b048657ba4126b6516e14402e5d
Vignesh Kalyanasundaram <vignesh20092001@gmail.com>
2025-07-05 10:43:17 -0700

    Week Data variables

---
+++ mydatajournal.js
@@ -0,0 +1,9 @@
+const weekData = [
+  { day: "Monday", sleepHours: 7, screenTime: 5, mood: "focused", caffeineIntake: 1, focusLevel: 8 },
+  { day: "Tuesday", sleepHours: 6.5, screenTime: 6, mood: "tired", caffeineIntake: 2, focusLevel: 6 },
+  { day: "Wednesday", sleepHours: 8, screenTime: 8, mood: "productive", caffeineIntake: 1, focusLevel: 9 },
+  { day: "Thursday", sleepHours: 6, screenTime: 4, mood: "distracted", caffeineIntake: 3, focusLevel: 5 },
+  { day: "Friday", sleepHours: 7.5, screenTime: 7, mood: "happy", caffeineIntake: 1, focusLevel: 8 },
+  { day: "Saturday", sleepHours: 9, screenTime: 3, mood: "relaxed", caffeineIntake: 0, focusLevel: 7 },
+  { day: "Sunday", sleepHours: 8.5, screenTime: 2, mood: "lazy", caffeineIntake: 0, focusLevel: 6 },
+];
